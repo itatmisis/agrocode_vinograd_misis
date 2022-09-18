c1dbb7adcb35e7aab1ed8ac6e95fe6cfa89bbd53
ilya <i.kiselevsky@gmail.com>
2022-09-18 17:59:28 +0300

    cors edited

--- app/services/main/presentation/main.py
+++ app/services/main/presentation/main.py
@@ -22,7 +22,7 @@ def create_app() -> FastAPI:
     fastapi_app.add_middleware(
         CORSMiddleware,
         allow_origins = ["*"],
-        allow_credentials = True,
+        allow_credentials = False,
         allow_methods = ["*"],
         allow_headers = ["*"]
     )
